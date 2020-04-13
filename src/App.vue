890cf4ab80aa6b819b17c13f830b8d114f79da64
karenheyn <karenheyn@gmail.com>
2020-04-13 00:05:42 -0400

    installed vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,32 @@
 <template>
   <div id="app">
-    <Header />
-    <AddToDo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import axios from "axios";
-import AddToDo from "./components/AddToDo";
-import Header from "./components/layout/Header";
-import Todos from "./components/Todos";
-
-export default {
-  name: "app",
-  components: {
-    Todos,
-    Header,
-    AddToDo
-  },
-  data() {
-    return {
-      todos: []
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      axios
-        .delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(
-          res => console.log(res),
-          (this.todos = this.todos.filter(todo => todo.id !== id))
-        )
-        .catch(err => console.log(err));
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios
-        .post("http://jsonplaceholder.typicode.com/todos", {
-          title,
-          completed
-        })
-        .then(res => (this.todos = [...this.todos, res.data]))
-        .catch(err => console.log(err));
-    }
-  },
-  created() {
-    axios
-      .get("http://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then(res => (this.todos = res.data))
-      .catch(err => console.log(err));
-  }
-};
-</script>
-
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0 auto;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
