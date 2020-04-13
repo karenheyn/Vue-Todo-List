6e7838a1afd7a21f21da1e3b8536e8debedfbf2d
karenheyn <karenheyn@gmail.com>
2020-04-12 22:47:26 -0400

    working on add todo bug

--- src/components/AddToDo.vue
+++ src/components/AddToDo.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <form>
-      <input type="text" name="title" placeholder="Add Todo..." />
+    <form @submit="addTodo">
+      <input
+        v-model="title"
+        type="text"
+        name="title"
+        placeholder="Add Todo..."
+      />
       <button type="submit" class="button">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
+import uuid from "uuid";
 export default {
-  name: "AddToDo"
+  name: "AddToDo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        id: uuid.v4(),
+        title: this.title,
+        completed: false
+      };
+      this.$emit("add-todo", newTodo);
+    }
+  }
 };
 </script>
 
